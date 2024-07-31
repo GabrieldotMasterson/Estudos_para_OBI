706afa8904fe4d96f70db930d1a028be1eb9b189
Masterson <gabriel.masterson2602@gmail.com>
2024-07-30 22:29:58 -0300

    att

--- SegundaFase/obi 2021/Retangulo.py
+++ SegundaFase/obi 2021/Retangulo.py
@@ -1,35 +1,23 @@
 import sys
+n_arvores = int(sys.stdin.readline())
 
-# Leitura da entrada
-n_arvores = int(sys.stdin.readline().strip())
-distancias = list(map(int, sys.stdin.readline().strip().split()))
-
-# Calcular a circunferência total
+distancias = list(map(int, sys.stdin.readline().split()))
 circunferencia_total = sum(distancias)
 
-# Construir prefix sums
-prefix_sums = [0] * (len(distancias) + 1)
-for i in range(len(distancias)):
-    prefix_sums[i + 1] = prefix_sums[i] + distancias[i]
+'''
+retangulo pares de lados com valores iguais
+8
+3 3 4 2 6 2 2 2
+'''
 
-# Função para calcular a soma de um subarray circular
-def soma_subarray(l, r):
-    if l <= r:
-        return prefix_sums[r + 1] - prefix_sums[l]
-    else:
-        return prefix_sums[len(distancias)] - prefix_sums[l] + prefix_sums[r + 1]
-
-# Função principal para verificar se é possível formar o retângulo
 def trabalhe():
-    half_circumference = circunferencia_total // 2
-    n = len(distancias)
-    for i in range(n):
-        for j in range(i + 1, n):
-            if j - i >= 2:
-                lado1 = soma_subarray(i, j - 1)
-                lado2 = soma_subarray(j, (i + n//2) % n - 1)
-                if lado1 == lado2 == half_circumference // 2:
-                    return "S"
+    for index, item in enumerate(distancias):
+        #print("index ", index, "item ", item)
+        if item + sum( distancias[(index+x)%len(distancias)] for x in range(len(distancias)//2)) == circunferencia_total//2:
+            if item == distancias[(index + len(distancias)//2)%len(distancias)]:
+                return "S"
+            elif item + distancias[index + 1] == distancias[(index + len(distancias)//2)%len(distancias)]:
+                return "S"
     return "N"
 
 print(trabalhe())
